1c48ad2462b9d4688b9842bc2d944930c6509ef9
apzarabi <apzarabi@gmail.com>
2015-07-21 02:21:01 +0430

    url bug fixed

--- sam/events/urls.py
+++ sam/events/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'event_small_card/$', views.event_small_card, name='event_small_card'),
+    url(r'event_small_card/$', views.event_cards, name='event_small_card'),
     url(r'event_row_verify/$', views.event_row_verify, name='event_row_verify'),
     url(r'event_row_condition/$', views.event_row_condition, name='event_row_condition'),
     url(r'event_row_purchase/$', views.event_row_purchase, name='event_row_purchase'),
