d46cfc7ddb42b99c27c9eeac0c48c1166d0c89d5
unknown <mmotiei@bitbucket.org>
2015-07-20 21:52:51 +0430

    first row card for verification by admin is now available

--- sam/events/urls.py
+++ sam/events/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'event_small_card/$', views.event_small_card, name='event_small_card')
+    url(r'event_small_card/$', views.event_small_card, name='event_small_card'),
+    url(r'event_row_verify/$', views.event_row_verify, name='event_row_verify')
 ]
--- sam/events/views.py
+++ sam/events/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def event_small_card(request):
     return render(request, 'event_small_card.html', {})
+
+
+def event_row_verify(request):
+    return render(request, 'event_row_verify.html', {})
