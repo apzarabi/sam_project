0605d6828f9ff78e504d3b50f96ec234eeeb9fb2
unknown <mmotiei@bitbucket.org>
2015-07-21 01:59:54 +0430

    all the horzontal event cards - ticket cards- (for verification, condition, purchase and printing) are ready now

--- sam/events/urls.py
+++ sam/events/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'event_small_card/$', views.event_small_card, name='event_small_card'),
-    url(r'event_row_verify/$', views.event_row_verify, name='event_row_verify')
+    url(r'event_row_verify/$', views.event_row_verify, name='event_row_verify'),
+    url(r'event_row_condition/$', views.event_row_condition, name='event_row_condition'),
+    url(r'event_row_purchase/$', views.event_row_purchase, name='event_row_purchase'),
+    url(r'event_row_print/$', views.event_row_print, name='event_row_print')
+
+
 ]
--- sam/events/views.py
+++ sam/events/views.py
@@ -9,3 +9,13 @@ def event_small_card(request):
 
 def event_row_verify(request):
     return render(request, 'event_row_verify.html', {})
+
+
+def event_row_condition(request):
+    return render(request, 'event_row_condition.html', {})
+
+def event_row_purchase(request):
+    return render(request, 'event_row_purchase.html', {})
+
+def event_row_print(request):
+    return render(request, 'event_row_print.html', {})
