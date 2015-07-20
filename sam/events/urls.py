6db52c82e279c618989074a8dffceaaaddc2b437
apzarabi <apzarabi@gmail.com>
2015-07-20 14:28:25 +0430

    start working on event_small_card. 2 images added.

--- sam/events/urls.py
+++ sam/events/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-
+    url(r'event_small_card/$', views.event_small_card, name='event_small_card')
 ]
--- sam/events/views.py
+++ sam/events/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def event_small_card(request):
+    return render(request, 'event_small_card.html', {})
