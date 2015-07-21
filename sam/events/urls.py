2162e7261ef43713da438d0179486a2f5038deee
unknown <mmotiei@bitbucket.org>
2015-07-21 06:15:40 +0430

    Event Information card is ready. DO NOT FORGET that you need to change header part for each page (take a look at test_event_cards)

--- sam/events/urls.py
+++ sam/events/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'event_row_purchase/$', views.event_row_purchase, name='event_row_purchase'),
     url(r'event_row_print/$', views.event_row_print, name='event_row_print'),
     url(r'event_cards/$', views.event_cards, name='event_cards'),
-    url(r'event_row_verify/$', views.event_row_verify, name='event_row_verify')
+    url(r'event_row_verify/$', views.event_row_verify, name='event_row_verify'),
 ]
