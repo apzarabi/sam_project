60f4b5f9dd9d85171af24b9e9417bf76e3ebaea1
unknown <mmotiei@bitbucket.org>
2015-08-03 18:44:35 +0430

    Persian numbers are ready

--- sam/events/views.py
+++ sam/events/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
 from events.models import *
 
 def event_cards(request):
     event = Event.objects.get(id=1)
+    cat = Category.objects.all()
     #return HttpResponse('<img class="img-responsive img-rounded" src="/media/pictures/77b6026e-b32f-4ae3-bb41-1719dcfca165.JPG">')
     print("here")
-    return render(request, 'test_event_cards.html', {'event': event})
+    return render(request, 'test_event_cards.html', {'event': event, 'categories': cat})
 
 
 def event_edit_page(request):
