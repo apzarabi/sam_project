4b7b75136c63c6699be464b148ce7342042fba75
unknown <mmotiei@bitbucket.org>
2015-08-03 19:06:11 +0430

    number big card

--- sam/events/templatetags/farsi_num.py
+++ sam/events/templatetags/farsi_num.py
@@ -4,10 +4,18 @@ from django.template.defaultfilters import stringfilter
 register = template.Library()
 
 @register.filter
-@stringfilter
 def farsi_num(eng_int):
+    print(eng_int)
     #devanagari_nums = ('०','१','२','३','४','५','६','७','८','९')
     farsi_num = ('۰', '١', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹')
     #     farsi_nums = (...)
     number = str(eng_int)
-    return ''.join(farsi_num[int(digit)] for digit in number)
+    ret = ""
+    for digit in number:
+        print(digit)
+        if digit in ('0','1','2','3','4','5','6','7','8','9'):
+            ret+=farsi_num[int(digit)]
+        else:
+            ret+=digit
+    print(ret)
+    return ret
